8c608bcb4f44540c0fce35677c1e249134a5da60
Akshay Nair <phenax5@gmail.com>
2020-05-22 14:26:34 +0530

    Adds message patch

--- config.def.h
+++ config.def.h
@@ -10,4 +10,13 @@ static const char *colorname[NUMCOLS] = {
 };
 
 /* treat a cleared input like a wrong password (color) */
-static const int failonclear = 1;
+static const int failonclear = 0;
+
+/* default message */
+static const char * message = "What's the password, dipshit?";
+
+/* text color */
+static const char * text_color = "#ffffff";
+
+/* text size (must be a valid size) */
+static const char * font_name = "-b&h-lucidatypewriter-medium-r-normal-sans-0-0-100-100-m-0-iso10646-1";
