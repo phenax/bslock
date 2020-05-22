88da20f5673b902e477777b030125aa80a1b9e26
Akshay Nair <phenax5@gmail.com>
2020-05-22 14:12:33 +0530

    Adds capslock color

--- config.def.h
+++ config.def.h
@@ -6,6 +6,7 @@ static const char *colorname[NUMCOLS] = {
 	[INIT] =   "black",     /* after initialization */
 	[INPUT] =  "#005577",   /* during input */
 	[FAILED] = "#CC3333",   /* wrong password */
+	[CAPS] = "red",         /* CapsLock on */
 };
 
 /* treat a cleared input like a wrong password (color) */
